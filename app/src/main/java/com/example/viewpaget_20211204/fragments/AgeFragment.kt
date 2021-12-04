0eac0264d99cfa1e02399f6bff90ca6fe5ad3110
김창배 <cbkim93@naver.com>
2021-12-04 13:50:51 +0900

    메인화면 - 뷰페이저 어댑터 객체화 - 뷰페이저와 연결

---
+++ app/src/main/java/com/example/viewpaget_20211204/fragments/AgeFragment.kt
@@ -0,0 +1,16 @@
+package com.example.viewpaget_20211204.fragments
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import com.example.viewpaget_20211204.R
+
+class AgeFragment : Fragment() {
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?
+    ): View? {
+        return inflater.inflate(R.layout.fragment_age, container, false)
+    }
+}

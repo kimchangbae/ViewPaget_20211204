df955439bb1dc531a5b7db4e2c90bc2ba11cd092
김창배 <cbkim93@naver.com>
2021-12-04 12:43:46 +0900

    이름 보여주는 프래그먼트 생성

---
+++ app/src/main/java/com/example/viewpaget_20211204/fragments/NameFragment.kt
@@ -0,0 +1,16 @@
+package com.example.viewpaget_20211204.fragments
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import com.example.viewpaget_20211204.R
+
+class NameFragment : Fragment() {
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?
+    ): View? {
+        return inflater.inflate(R.layout.fragment_name, container, false)
+    }
+}
